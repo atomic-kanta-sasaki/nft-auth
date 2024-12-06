44c58fa66b1b9e6d4267d65df0960b451f580d13
atomic-kanta-sasaki <kanta.sasaki.0111@gmail.com>
2024-12-06 13:43:21 +0900

    NFT認証用実装サンプル

--- application/app/api/controller/auth/nft/route.ts
+++ application/app/api/controller/auth/nft/route.ts
@@ -1,18 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest): Promise<NextResponse> {
-    
-    // request bodyとかに署名をもらう
-    // 署名データをbase64とかでエンコードするとか？
+export async function POST(reqest: NextRequest): Promise<NextResponse> {
+  // ほんとはこのシステム固有の識別子ではなく、連携先と共有しているemailとかがいいけど、今カラムがないのでいったんUserIdを使用
+  const { userId } = await reqest.json();
+  const prisma = new PrismaClient();
+  
+  // 認証対象のユーザー情報の確認(認証用のJWTとかで良い)
+  // ユーザーの登録済みのEOAを確認
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId
+    },
+    include: {
+      eoa: true
+    }
+  })
+  // EOAが対象のNFTを持っているかどうか確認
+  const eoaList = user?.eoa.map(e => e.address);
 
-    // 署名の検証を行う
-    // 検証結果からEOAアドレスを取得する
+  if(!eoaList || eoaList?.length === 0) {
+    NextResponse.json(          
+      { error: "Unauthorized: No linked EOA found" },
+      { status: 401 }
+    )
+  }
 
-    // EOAアドレスから認証対処のNFTを持っているかどうか判定
+  // EOAアドレスから認証対処のNFTを持っているかどうか判定
 
-    // 持っていれば認証成功
-    // 持っていなければ認証失敗
-    
-    return NextResponse.json("authrizaton")
+  // 持っていれば認証成功
+  // 持っていなければ認証失敗
+  
+  return NextResponse.json("authrizaton")
 }
 
